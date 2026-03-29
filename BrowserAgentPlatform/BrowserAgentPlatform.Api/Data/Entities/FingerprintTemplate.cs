0a0f13228a688f2d6624d412e1cb9935cfe0950a
bubuwobu <710821246@qq.com>
2026-03-29 11:34:24 +0800

    删除无效代码

--- BrowserAgentPlatform/BrowserAgentPlatform.Api/Data/Entities/FingerprintTemplate.cs
+++
@@ -1,8 +0,0 @@
-namespace BrowserAgentPlatform.Api.Data.Entities;
-public class FingerprintTemplate
-{
-    public long Id { get; set; }
-    public string Name { get; set; } = "";
-    public string ConfigJson { get; set; } = "{}";
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-}
--- BrowserAgentPlatform/BrowserAgentPlatform.Web/src/services/auth.js
+++
@@ -1,6 +0,0 @@
-const key = 'browser-agent-token'
-export const auth = {
-  token: () => localStorage.getItem(key),
-  set: (token) => localStorage.setItem(key, token),
-  clear: () => localStorage.removeItem(key)
-}
